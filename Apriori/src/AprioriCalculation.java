6912a594804303f5b8367fa68353cb45efd2b904
Sawyer Jager <sawyer.jgr@gmail.com>
2014-05-01 00:08:59 -0500

    LOOK GUYS I'M SAWYER I'M TOO COOL FOR CURLY BRACES

--- Apriori/src/AprioriCalculation.java
+++ Apriori/src/AprioriCalculation.java
@@ -13,15 +13,19 @@ class AprioriCalculation
 {   
     Vector<String> candidates=new Vector<String>(); //the current candidates 
     String configFile="config.txt"; //configuration file
-    String transaFile="transa.txt"; //transaction file
+    String transaFile="transa (1).txt"; //transaction file
+    String sentimentFile="sentiment.csv";
     String outputFile="apriori-output.txt";//output file
     
     int maxItemID;
     int numTransactions; //number of transactions
     double minSup; //minimum support for a frequent itemset
+    HashMap<String, Integer> sentiments;
     String itemSep = " "; //the separator value for items in the database
 
     Vector<String> frequentCandidates = new Vector<String>(); //the frequent candidates for the current itemset
+    Vector<int[]> frequentReviews = new Vector<int[]>(); 
+    
     FileInputStream file_in; //file input stream
     BufferedReader data_in; //data input stream
     FileWriter fw;
@@ -32,6 +36,22 @@ class AprioriCalculation
         Date d; //date object for timing purposes
         long start, end; //start and end time
         int itemsetNumber=0; //the current itemset being looked at
+        
+        sentiments = new HashMap<String, Integer>();
+        try{
+        	file_in = new FileInputStream(sentimentFile);
+        	data_in = new BufferedReader(new InputStreamReader(file_in));
+        	
+        	String current;
+            while((current = data_in.readLine()) != null)
+            {
+            	sentiments.put(current.split("\t")[0], Integer.parseInt(current.split("\t")[1]));
+            }
+        } catch (Exception e)
+        {}
+        
+        
+        
         
         maxItemID=999;
 
@@ -118,7 +138,18 @@ class AprioriCalculation
                     
 		        	for(int i=0; i<numTransactions; i++)
 		        	{
-		        		List<String> items = Arrays.asList(data_in.readLine().split(itemSep));
+		        		String pattern = "(\\}?\\{)|\\}";
+	                	String[] objectList = data_in.readLine().split(pattern);
+	                	String itemList = "";
+	                	String reviewList = "";
+	                	for(int j=1; j<objectList.length-1; j++) {
+	                		itemList += objectList[j].split(",")[0];
+	                		itemList += " ";
+	                		reviewList += objectList[j].split(",")[1];
+	                		reviewList += ",";
+	                	}
+	                	String weekday = objectList[objectList.length-1];
+		        		List<String> items = Arrays.asList(itemList.split(itemSep));
 		        		for(int j=0; j<items.size(); j++)
 		        		{
 		        			if(candidates.contains(items.get(j)))
@@ -150,7 +181,18 @@ class AprioriCalculation
                     
 		        	for(int i=0; i<numTransactions; i++)
 		        	{
-		        		List<String> items = Arrays.asList(data_in.readLine().split(itemSep));
+		        		String pattern = "(\\}?\\{)|\\}";
+	                	String[] objectList = data_in.readLine().split(pattern);
+	                	String itemList = "";
+	                	String reviewList = "";
+	                	for(int j=1; j<objectList.length-1; j++) {
+	                		itemList += objectList[j].split(",")[0];
+	                		itemList += " ";
+	                		reviewList += objectList[j].split(",")[1];
+	                		reviewList += ",";
+	                	}
+	                	String weekday = objectList[objectList.length-1];
+		        		List<String> items = Arrays.asList(itemList.split(itemSep));
 		        		for(int j=0; j<items.size(); j++)
 		        		{
 	        				for(int k=j+1; k<items.size(); k++)
@@ -184,9 +226,10 @@ class AprioriCalculation
 
     private void calculateFrequentItemsets(int n)
     {
-    	StringTokenizer st, stFile; //tokenizer for candidate and transaction
+    	StringTokenizer st, stFile, stReview; //tokenizer for candidate and transaction
         boolean match; //whether the transaction has all the items in an itemset
         int count[] = new int[candidates.size()]; //the number of successful matches
+        int review[][] = new int[candidates.size()][7];
 
         try
         {
@@ -198,31 +241,61 @@ class AprioriCalculation
                 data_in = new BufferedReader(new InputStreamReader(file_in));
                 
                 //for each transaction
+                System.out.println(candidates.size());
                 for(int i=0; i<numTransactions; i++)
                 {
-                    stFile = new StringTokenizer(data_in.readLine(), itemSep); //read a line from the file to the tokenizer
-                    boolean trans[] = new boolean[maxItemID+1];
+                	String pattern = "(\\}?\\{)|\\}";
+                	String[] objectList = data_in.readLine().split(pattern);
+                	String itemList = "";
+                	String reviewList = "";
+                	for(int j=1; j<objectList.length-1; j++) {
+                		itemList += objectList[j].split(",")[0];
+                		itemList += " ";
+                		reviewList += objectList[j].split(",")[1];
+                		reviewList += ",";
+                	}
+                	String weekday = objectList[objectList.length-1];
+                	
+                	System.out.println("Transaction " + i + ": " + itemList + weekday);
+                	
+                	stFile = new StringTokenizer(itemList, itemSep); //read a line from the file to the tokenizer
+                    stReview = new StringTokenizer(reviewList, ",");
+                	boolean trans[] = new boolean[maxItemID+1];
+                    int reviewNumber[][] = new int[maxItemID+1][7];
                     
                     while(stFile.hasMoreTokens())
                     {
-                        trans[Integer.parseInt(stFile.nextToken())] = true;
+                        int itemID = Integer.parseInt(stFile.nextToken());
+                    	trans[itemID] = true;
+                        reviewNumber[itemID][GetWeekdayIndex(weekday)] = calculateSentiment(stReview.nextToken());
+                        System.out.println("\tItem:" + itemID + " " + reviewNumber[itemID]);
                     }
                     
                     //check each candidate
                     for(int c=0; c<candidates.size(); c++)
                     {
                         match = false; //reset match to false
+                        int[] totalSentiment = new int[7];
                         //tokenize the candidate so that we know what items need to be present for a match
                         st = new StringTokenizer(candidates.get(c), ", ");
                         //check each item in the itemset to see if it is present in the transaction
                         while(st.hasMoreTokens())
                         {
-                            match = (trans[Integer.valueOf(st.nextToken())]);
+                        	int itemID = Integer.valueOf(st.nextToken());
+                            match = trans[itemID];
+                            for(int k=0; k<7; k++){
+	                            totalSentiment[k] += reviewNumber[itemID][k];
+	                        }
                             if(!match) //if it is not present in the transaction stop checking
                                 break;
                         }
-                        if(match) //if at this point it is a match, increase the count
+                        
+                        if(match) {//if at this point it is a match, increase the count
                             count[c]++;
+                        	for(int m=0; m<7; m++){
+                        		review[c][m] += totalSentiment[m];
+                        	}
+                        }
                     }
                     
                 }
@@ -232,9 +305,11 @@ class AprioriCalculation
                     if(count[i]>=minSup)
                     {
                         frequentCandidates.add(candidates.get(i));
+                        frequentReviews.add(review[i]);
                         //put the frequent itemset into the output file
+                        System.out.println("( "+ candidates.get(i) + ") " + review[i][0] + " " + review[i][1] + " " + review[i][2] + " " + review[i][3] + " " + review[i][4] + " " + review[i][5] + " " + review[i][6]);
                         if(n==minSup)
-                        	file_out.write("("+ candidates.get(i) + ") " + count[i] + "\n");
+                        	file_out.write("( "+ candidates.get(i) + ") " + review[i][0] + " " + review[i][1] + " " + review[i][2] + " " + review[i][3] + " " + review[i][4] + " " + review[i][5] + " " + review[i][6] + "\n");
                     }
                 }
                 file_out.close();
@@ -249,5 +324,34 @@ class AprioriCalculation
         //new candidates are the old frequent candidates
         candidates = new Vector<String>(frequentCandidates);
         frequentCandidates.clear();
+        frequentReviews.clear();
+    }
+    
+    public int GetWeekdayIndex(String input) {
+    	int result = 0;
+    	if(input.equals("Sunday"))
+    		result = 0;
+    	else if (input.equals("Monday"))
+    		result = 1;
+    	else if (input.equals("Tuesday"))
+    		result = 2;
+    	else if (input.equals("Wednesday"))
+    		result = 3;
+    	else if (input.equals("Thursday"))
+    		result = 4;
+    	else if (input.equals("Friday"))
+    		result = 5;
+    	else if (input.equals("Saturday"))
+    		result = 6;
+    	return result;
+    }
+    
+    public int calculateSentiment(String input) {
+    	int sum = 0;
+    	StringTokenizer tok = new StringTokenizer(input, " ");
+    	while(tok.hasMoreTokens()) {
+    		sum += sentiments.get(tok.nextToken());
+    	}
+    	return sum;
     }
 }
