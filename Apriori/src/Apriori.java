915c7a1d3724fea8736d767d389f0546361f5a48
Rees Klintworth <reesk5150@gmail.com>
2014-05-01 16:59:20 -0500

    Updated Java Code.

--- Apriori/src/Apriori.java
+++ Apriori/src/Apriori.java
@@ -2,7 +2,7 @@ public class Apriori
 {
     public static void main(String[] args) 
     {
-        AprioriCalculator ap = new AprioriCalculator(3000, 999, 3, "transactions.txt", "project_2_output.txt");
-        ap.mainApriori();
+        AprioriCalculator ap = new AprioriCalculator(3000, 999, 3, "transactions.txt", "sentiment.csv", "apriori_output.txt");
+    	ap.mainApriori();
     }
 }
